2dfa9539557da8cbf1466ee34d1812ac017d890f
Wilson Hsieh <wilson.hsieh@bitsxbites.com>
2017-06-01 15:11:58 +0800

    modify via flake 8

--- media.py
+++ media.py
@@ -1,8 +1,18 @@
 import webbrowser
 
 class Movie():
+	""" Movie  object
+    """
 
 	def __init__(self , movie_title, movie_storyline , movie_post_img_url , movie_youtube_url, start_date):
+		""" __init__
+	    Keyword arguments:
+	    movie_title -- movie title
+	    movie_storyline -- movie storyline
+	    movie_post_img_url -- movie youtube url
+	    start_date -- start date
+	    """
+
 		print 'init ...'
 		self.title = movie_title
 		self.storyline = movie_storyline
@@ -11,6 +21,9 @@ class Movie():
 		self.start_date = start_date
 
 	def show_trailer(self):
+		""" show_trailer
+	    web browse ropen trailer youtube url
+	    """
 		webbrowser.open(self.trailer_youtube_url)
 
 
